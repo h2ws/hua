0416a4e052d3b0ba73b1d832650f891e8e3bd489
oyydd <oyy284688@gmail.com>
2020-11-29 10:42:30 +0800

    move Company class to Customer.java file

--- Customer.java
+++ Customer.java
@@ -32,3 +32,22 @@ class Customer {
         return inputTag;
     }
 }
+
+class Company extends Insurable {
+    private String companyName;
+
+    static final String inputTag = "COMPANY";
+
+    Company(HashMap<String, ArrayList<Tag>> tags) throws ParseException {
+        super(tags);
+        companyName = tags.get("COMPANY_NAME").get(0).getValue();
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public static String getInputTag() {
+        return inputTag;
+    }
+}
